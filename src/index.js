185b5ea40da5daa90a87f774598ceb38eb2b60f3
redy-indrawan <redy.indrawan@gmail.com>
2021-10-28 17:15:47 +0700

    Adding Babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const tes = {
+const Testing = {
     tes: 1,
     tes2: 2,
+    tes3: 3
 };
 
-console.log(tes);
+const Testing2 = {
+    ...Testing,
+    tes4: 4,
+    tes3: 10
+}
+
+console.log(Testing);
+console.log(Testing2);
